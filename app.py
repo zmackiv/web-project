fb4f0de97311854e437b9027c89a114652c82690
Jan K <xkostrh2@mendelu.cz>
2023-11-18 15:47:26 +0100

    test

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def view_homepage():
+    return render_template("index.jinja")
+
+@app.route("/products")
+def view_products_page():
+    return render_template("products.jinja")
+
+@app.route("/contacts")
+def view_contact_page():
+    return render_template("contacts.jinja")
+
+# First endpoint
+@app.route("/welcome")
+def view_welcome_page():
+    return render_template("welcome_page.jinja", name="Mr Yoda")
 
 
-if __name__ == '__main__':
-    app.run()
+app.run('0.0.0.0', port=5000)
