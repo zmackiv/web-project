0ebecc5912f5b8616cda779d4c97e55e80768114
ZoryanaMackiv <xmackiv@mendelu.cz>
2023-12-28 18:29:47 +0100

    Přidání category service

---
+++ service/category_service.py
@@ -0,0 +1,19 @@
+from database.database import get_db
+
+
+class CategoryService:
+
+    @staticmethod
+    def get_all():
+        db = get_db()
+        return db.execute(
+            "SELECT * FROM typy_stroje"
+        ).fetchall()
+
+    @staticmethod
+    def get_by_id(id: int):
+        db = get_db()
+        return db.execute(
+            "SELECT * FROM typy_stroje WHERE id_typstroje = ?",
+            [id]
+        ).fetchone()
