c499bc452374d21b694612dff6f6456323488998
ZoryanaMackiv <xmackiv@mendelu.cz>
2024-01-04 22:14:55 +0100

    přidání kalendáře na vybírání data

--- static/script.js
+++ static/script.js
@@ -6,3 +6,14 @@ document.getElementById("hamburger")
     .addEventListener("click", toggleMenu)
 document.getElementById("nav")
     .addEventListener("click", toggleMenu)
+
+
+$(function () {
+        $("#datepicker").datepicker({
+            dateFormat: "dd-mm-yy", // Formát data (změňte podle potřeby)
+            onSelect: function (dateText) {
+                $("#datum").val(dateText); // Uloží vybrané datum do skrytého pole s id 'datum'
+            }
+        });
+        $("#datepicker").datepicker("show");
+    });
